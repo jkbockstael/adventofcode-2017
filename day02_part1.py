21c259694d151b1c6d6548655a5f26e623e9b96b
Jean-Karim Bockstael <jkb@jkbockstael.be>
2017-12-02 09:31:32 +0100

    Solution for Day 2 part 1

---
+++ day02_part1.py
@@ -0,0 +1,17 @@
+# Advent of Code 2017 - Day 2 - Corruption Checksum
+# http://adventofcode.com/2017/day/2
+
+import sys
+import re
+
+def parse_input(lines):
+    return [parse_line(line) for line in lines]
+
+def parse_line(line):
+    return [int(x) for x in re.split('\s', line.rstrip())]
+
+def checksum(rows):
+    return sum([max(row) - min(row) for row in rows])
+
+rows = parse_input(sys.stdin.readlines())
+print(checksum(rows))
