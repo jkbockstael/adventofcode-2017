a467411c112d2aff4d2ddda73194cfe6b89ce196
Jean-Karim Bockstael <jkb@jkbockstael.be>
2017-12-07 07:40:29 +0100

    Solution for Day 6 part 1

---
+++ day06_part1.py
@@ -0,0 +1,26 @@
+# Advent of Code 2017 - Day 6 - Memory Reallocation
+# http://adventofcode.com/2017/day/6
+
+import re
+
+def reallocate(memory):
+    cell = memory.index(max(memory))
+    value = memory[cell]
+    memory[cell] = 0
+    while value > 0:
+        cell = (cell + 1) % len(memory)
+        memory[cell] = memory[cell] + 1
+        value = value - 1
+    return memory
+
+def steps_before_cycle(memory):
+    steps = 0
+    visited = []
+    while memory not in visited:
+        visited.append(memory)
+        memory = reallocate(memory[:])
+        steps = steps + 1
+    return steps
+
+memory = [int(x) for x in re.split('\s+', input())]
+print(steps_before_cycle(memory))
